192fa162c7c0fd77b9fdc0e70ac9f6349812cc1d
zltvk <o.zlotovik@gmail.com>
2025-09-09 19:03:54 +0200

    v3

--- task_2.py
+++ task_2.py
@@ -3,7 +3,7 @@ import random
 def get_numbers_ticket(min, max, quantity=1):
     try:
         #Validating parameters
-        if min < 1 or max > 100 or quantity < 1 or quantity > (max - min + 1):
+        if min < 1 or max > 1000 or quantity < 1 or quantity > (max - min + 1):
             return []
         #Generation
         result = sorted(random.sample(range(min, max+1),quantity))
